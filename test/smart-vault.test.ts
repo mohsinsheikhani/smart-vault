dd658d666066275e842a1945ff2d9c04927b93a4
bugattie <m.mohsinbarkaati@gmail.com>
2025-02-25 15:26:55 +0500

    Initial commit

---
+++ test/smart-vault.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SmartVault from '../lib/smart-vault-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/smart-vault-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SmartVault.SmartVaultStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
